4dd6dbc91de742edf3265cbe9d7cbeb2e88d5983
Jamison Fox <fox.jamison@outlook.com>
2022-11-07 10:05:06 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styled from 'styled-components'
+import Button from './components/Button';
+
+const WrapperDiv = styled.div`
+  font-family: sans-serif;
+  text-align: center;
+`;
+
+const BlueH1 = styled.h1`
+  color: ;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <WrapperDiv>
+      <BlueH1>Styled Components Playground</BlueH1>
+      <Button type='primary'>Primary</Button>
+      <Button type='success'>Success</Button>
+      <Button type='goats'>Primary</Button>
+      <Button type='laser'>Primary</Button>
+    </WrapperDiv>
   );
 }
 
